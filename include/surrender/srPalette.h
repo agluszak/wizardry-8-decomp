45cd168d6a4d9903ad7ccb41e13bb9d8807f9824
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-30 18:00:34 +0200

    Recover srPalette storage and ownership API

--- include/surrender/srPalette.h
+++ include/surrender/srPalette.h
@@ -11,6 +11,8 @@
 class srPalette
     : public srClassSupport<srPalette, srClass, 0, 0x2900> {
 public:
+    class Quantizer;
+
     static SR_DLL_IMPORT const char* sGetClassName();
     static SR_DLL_IMPORT srPalette* findMatchingPalette(
         const srARGB* colors, long color_count);
@@ -22,11 +24,34 @@ public:
     virtual SR_DLL_IMPORT void dump(std::ostream& stream) override;
     virtual SR_DLL_IMPORT srClass* vInstance() override;
 
+    SR_DLL_IMPORT srARGB getColor(long index) const;
+    SR_DLL_IMPORT const srARGB* getPaletteDataPtr();
+    SR_DLL_IMPORT long getPaletteSize() const;
+    SR_DLL_IMPORT int matchPalette(
+        const srARGB* colors, long color_count) const;
+    SR_DLL_IMPORT unsigned char quantize(const srARGB& color);
+    SR_DLL_IMPORT void quantize(
+        unsigned char* indices,
+        const srARGB* colors,
+        long color_count);
+    SR_DLL_IMPORT void releaseQuantizer();
+    SR_DLL_IMPORT void setColor(long index, const srARGB& color);
+    SR_DLL_IMPORT void setColors(
+        long destination_index,
+        const srARGB* colors,
+        long color_count);
+    SR_DLL_IMPORT void update();
+
 protected:
     virtual SR_DLL_IMPORT ~srPalette() override;
 
 private:
-    unsigned char palette_state_18[0x10];
+    SR_DLL_IMPORT void updateQuantizer();
+
+    unsigned long flags_18;
+    srARGB* colors_1c;
+    long color_count_20;
+    Quantizer* quantizer_24;
 };
 
 static_assert(sizeof(srPalette) == 0x28, "srPalette_must_be_0x28");
