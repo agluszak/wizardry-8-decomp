ab185343b53af2afc288fb2ec1d895a67bb10b2b
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-30 14:53:30 +0200

    Complete the srHeap allocation foundation

--- include/surrender/srHeap.h
+++ include/surrender/srHeap.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #if defined(_MSC_VER) && !defined(SURRENDER_BUILD)
 #define SR_DLL_IMPORT __declspec(dllimport)
 #else
@@ -8,8 +10,15 @@
 
 class srHeap {
 public:
+    SR_DLL_IMPORT srHeap();
+    SR_DLL_IMPORT ~srHeap();
+
     SR_DLL_IMPORT void* allocate(unsigned long size);
     SR_DLL_IMPORT void free(void* allocation);
+    SR_DLL_IMPORT void free(void* allocation, unsigned int size);
+    SR_DLL_IMPORT void freeAll();
+    SR_DLL_IMPORT unsigned long msize(void* allocation);
+    SR_DLL_IMPORT void dump(std::ostream& stream);
 };
 
 extern SR_DLL_IMPORT class srHeap srHeap;
