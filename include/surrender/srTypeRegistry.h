1d4e332ef7e01139513211a139f57e0ecb4f8142
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-30 14:41:29 +0200

    Complete the SurRender runtime-class foundation

--- include/surrender/srTypeRegistry.h
+++ include/surrender/srTypeRegistry.h
@@ -11,7 +11,23 @@ class srRegistry {
 public:
     class ClassNode;
 
+    SR_DLL_IMPORT srRegistry();
+    SR_DLL_IMPORT ~srRegistry();
+    SR_DLL_IMPORT srRegistry& operator=(const srRegistry& other);
+
+    SR_DLL_IMPORT unsigned long allocateID();
+    SR_DLL_IMPORT int checkValidity();
+    SR_DLL_IMPORT void dumpClassHierarchy(std::ostream& stream);
+    SR_DLL_IMPORT void dumpInstanceNames(
+        ClassNode* node, std::ostream& stream, int indent);
     SR_DLL_IMPORT ClassNode* getClassNode(unsigned long class_id);
+    SR_DLL_IMPORT unsigned long getClassID(ClassNode* node);
+    SR_DLL_IMPORT const char* getClassName(ClassNode* node);
+    SR_DLL_IMPORT ClassNode* getChildClass(ClassNode* parent, ClassNode* child);
+    SR_DLL_IMPORT long getNumberOfInstances(ClassNode* node, int recursive);
+    SR_DLL_IMPORT ClassNode* getRootClass();
+    SR_DLL_IMPORT ClassNode* getRootNode();
+    SR_DLL_IMPORT int isDerivedOrSame(ClassNode* derived, ClassNode* base);
     SR_DLL_IMPORT ClassNode* registerClass(
         const char* class_name,
         ClassNode* parent,
@@ -25,6 +41,18 @@ public:
         ClassNode* node,
         const char* name,
         const srRuntimeClass* relative_to);
+    SR_DLL_IMPORT srRuntimeClass* find(
+        ClassNode* node, const srRuntimeClass* relative_to);
+    SR_DLL_IMPORT srRuntimeClass* find(ClassNode* node, unsigned long id);
+    SR_DLL_IMPORT srRuntimeClass* findExact(
+        ClassNode* node,
+        const char* name,
+        const srRuntimeClass* relative_to);
+    SR_DLL_IMPORT srRuntimeClass* findExact(
+        ClassNode* node, const srRuntimeClass* relative_to);
+    SR_DLL_IMPORT srRuntimeClass* findExact(ClassNode* node, unsigned long id);
+    SR_DLL_IMPORT void refreshInstance(
+        ClassNode* node, srRuntimeClass* instance);
 };
 
 class srRuntimeClass {
@@ -39,16 +67,49 @@ public:
     virtual SR_DLL_IMPORT void dump(std::ostream& stream);
     virtual SR_DLL_IMPORT void verify(e_verify mode);
 
+    static SR_DLL_IMPORT srRegistry::ClassNode* sGetClassNode();
+    static SR_DLL_IMPORT long getTotalInstances(int recursive);
+    static SR_DLL_IMPORT void dumpNames(std::ostream& stream, int indent);
+
+    SR_DLL_IMPORT srRuntimeClass(const srRuntimeClass& other);
+    SR_DLL_IMPORT srRuntimeClass& operator=(const srRuntimeClass& other);
     SR_DLL_IMPORT void setName(const char* name);
     SR_DLL_IMPORT const char* getName() const;
+    SR_DLL_IMPORT unsigned long getID() const;
+    SR_DLL_IMPORT void getUniqueName(std::ostream& stream) const;
+    SR_DLL_IMPORT int isNamed() const;
+    SR_DLL_IMPORT int matchClassID(unsigned long class_id) const;
 
 protected:
+    SR_DLL_IMPORT srRuntimeClass();
     virtual SR_DLL_IMPORT ~srRuntimeClass();
+
+private:
+    char* name_04;
+    unsigned long id_08;
 };
+
+static_assert(sizeof(srRuntimeClass) == 0x0c,
+              "srRuntimeClass_must_be_0x0c");
 
 class srClass : public srRuntimeClass {
 public:
+    typedef void (__cdecl *UpdateCallBack)(
+        srClass* instance, double time, double elapsed);
+
+    static SR_DLL_IMPORT const char* sGetClassName();
     static SR_DLL_IMPORT srRegistry::ClassNode* sGetClassNode();
+    static SR_DLL_IMPORT srClass* find(unsigned long id);
+    static SR_DLL_IMPORT srClass* find(
+        const char* name,
+        unsigned long class_id,
+        const srRuntimeClass* relative_to);
+    static SR_DLL_IMPORT srClass* find(
+        const char* name, const srClass* relative_to);
+    static SR_DLL_IMPORT srClass* find(const srClass* relative_to);
+    static SR_DLL_IMPORT void performUpdates(double time);
+
+    SR_DLL_IMPORT srClass(const srClass& other);
     SR_DLL_IMPORT srClass& operator=(const srClass& other);
 
     virtual const char* getClassName() const override = 0;
@@ -63,11 +124,18 @@ protected:
 public:
     virtual srClass* vInstance() = 0;
 
+    SR_DLL_IMPORT srClass* clone();
+    SR_DLL_IMPORT srClass* instance();
     SR_DLL_IMPORT int release() const;
     SR_DLL_IMPORT void addReference() const;
     SR_DLL_IMPORT long getReferenceCount() const;
     SR_DLL_IMPORT void autoRelease();
     SR_DLL_IMPORT void touch();
+    SR_DLL_IMPORT unsigned long getTimestamp() const;
+    SR_DLL_IMPORT UpdateCallBack getUpdateCallBack();
+    SR_DLL_IMPORT double getUpdateInterval();
+    SR_DLL_IMPORT void setUpdate(UpdateCallBack callback, double interval);
+    SR_DLL_IMPORT void setUpdatesTime(double time);
     void* operator new(unsigned int size) { return srHeap.allocate(size); }
 
     /* Every class in this hierarchy is freed through the SurRender heap rather
@@ -81,11 +149,14 @@ public:
 
 protected:
     SR_DLL_IMPORT srClass();
+    SR_DLL_IMPORT unsigned long allocateTimeStamps(unsigned long count) const;
 
 private:
-    /* The imported srClass constructor owns this runtime bookkeeping. Direct
-       first-party subclasses begin their own storage at +0x18. */
-    unsigned char runtime_state_004_[0x14];
+    struct Update;
+
+    long reference_count_0c;
+    unsigned long timestamp_10;
+    Update* update_14;
 };
 
 static_assert(sizeof(srClass) == 0x18, "srClass_must_be_0x18");
