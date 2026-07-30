0777aeab39f5402d515e9907db628931a8d430d8
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-30 15:36:54 +0200

    Complete the srIStreamOpener foundation

--- include/surrender/srIStreamOpener.h
+++ include/surrender/srIStreamOpener.h
@@ -6,10 +6,36 @@ class srIStreamOpener {
 public:
     class __declspec(novtable) Opener {
     public:
-        virtual SR_DLL_IMPORT ~Opener() {}
+        SR_DLL_IMPORT Opener();
+        virtual SR_DLL_IMPORT ~Opener();
+        SR_DLL_IMPORT Opener& operator=(const Opener& other);
+
         virtual srBinIStream* open(const char* path) = 0;
         virtual const char* getDescription() const = 0;
     };
 
+    SR_DLL_IMPORT srIStreamOpener();
+    SR_DLL_IMPORT ~srIStreamOpener();
+    SR_DLL_IMPORT srIStreamOpener& operator=(const srIStreamOpener& other);
+
     SR_DLL_IMPORT void addStreamType(Opener* opener, const char* extension);
+    SR_DLL_IMPORT srBinIStream* open(const char* path);
+
+private:
+    struct StreamType;
+
+    SR_DLL_IMPORT Opener* findOpener(const char* extension);
+    SR_DLL_IMPORT srBinIStream* open(
+        const char* path, const char* extension);
+    SR_DLL_IMPORT void parsePrefix(
+        char** prefix, char** path, const char* input);
+
+    long count_00;
+    StreamType* first_04;
+    StreamType* end_08;
 };
+
+static_assert(sizeof(srIStreamOpener::Opener) == 0x04,
+              "srIStreamOpener_Opener_must_be_0x04");
+static_assert(sizeof(srIStreamOpener) == 0x0c,
+              "srIStreamOpener_must_be_0x0c");
