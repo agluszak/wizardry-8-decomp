d5f0df9070c00619b9d75a0e216ae48f9d7046e3
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-30 16:47:34 +0200

    Recover the srStatisticsManager counter layout

--- include/surrender/srStatisticsManager.h
+++ include/surrender/srStatisticsManager.h
@@ -8,7 +8,18 @@ class srStatisticsManager {
 public:
     struct Statistics {
         double elapsed_time_00;
-        unsigned long values_08[12];
+        unsigned long meshes_traversed_08;
+        unsigned long meshes_submitted_0c;
+        unsigned long triangles_submitted_10;
+        unsigned long triangles_after_culling_14;
+        unsigned long vertices_submitted_18;
+        unsigned long vertices_after_culling_1c;
+        unsigned long material_processing_stalls_20;
+        unsigned long diffuse_operations_24;
+        unsigned long specular_operations_28;
+        unsigned long alpha_operations_2c;
+        unsigned long fog_operations_30;
+        unsigned long texture_coordinate_operations_34;
     };
 
     SR_DLL_IMPORT void reset();
