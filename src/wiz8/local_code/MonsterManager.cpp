98b56945c3efe0d48765b861d033b1d9d696b172
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-09-09 03:12:00 +0200

    Recover monster alternate-name buffer

--- src/wiz8/local_code/MonsterManager.cpp
+++ src/wiz8/local_code/MonsterManager.cpp
@@ -22,6 +22,8 @@ W8MonsterRecord* g_monster_record_cache[1000];
 float g_monster_record_float_scale = 20.0f;
 // GLOBAL: WIZ8 0x00683698
 int g_monster_info_iterator_index;
+// GLOBAL: WIZ8 0x006875c3
+W8WideChar g_monster_name_buffer[22];
 // GLOBAL: WIZ8 0x006875ef
 unsigned char g_alternate_name_slot;
 }
