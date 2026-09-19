3a2921e9c973fe1ec76b6d4ab0b2e44b9167bb2a
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-09-19 03:31:31 +0200

    Recover Gameplay Init.cpp translation unit

--- include/wiz8/local_code/GameplayDatabase.h
+++ include/wiz8/local_code/GameplayDatabase.h
@@ -3,8 +3,6 @@
 #include "wiz8/layouts/game_status.h"
 #include "wiz8/layouts/gameplay_databases.h"
 
-void ResetGameStatus(unsigned char release);
-void InitializeGameplayRuntimeObjects(void);
 unsigned char LoadMonsterDatabase(W8MonsterRecord** records);
 unsigned char LoadMonsterDatabaseRange(unsigned int uiStartIndex, unsigned int uiEndIndex,
                                        unsigned int unused, W8MonsterRecord* records);
@@ -16,26 +14,10 @@ extern W8GameTimer* g_gameplay_timer_685067;
 extern unsigned char g_party_moving_006850b5;
 extern unsigned int g_starting_item_ids[];
 
-unsigned char InitializeFactDatabase(void);
 unsigned char InitializeItemDatabase(void);
-unsigned char InitializeLevelDatabase(void);
 unsigned char InitializeItemTables(void);
-unsigned char InitializeNpcDatabase(void);
-void DestroyFactDatabase(void);
 void DestroyItemDatabase(void);
-void DestroyLevelDatabase(void);
 void DestroyItemTables(void);
-void DestroyNpcDatabase(void);
 void FreeIfNotNull(void* block);
-unsigned char AllocateStatusBuffers(W8StatusBuffers* status);
-void FreeStatusBuffers(W8StatusBuffers* status);
-void ResetPartySlotRow(int slot);
-void ResetGameplayStatusBlock(void);
-void ResetTargetingState(void);
-void DestroyGameplayObjects(void);
-void ResetForNewGame(void);
-void ResetGameplaySlot(unsigned int slot);
-void ResetGameplaySettings(void);
 
-void RunNewGameOpeningSequence(unsigned char notify, const wchar_t* target);
 void QueueGameplayEvent(int event_type, int party_slot);
