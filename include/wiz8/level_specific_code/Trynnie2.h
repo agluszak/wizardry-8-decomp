27ceb0b8301cb453d5a11b807941cc59a673a7a1
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-09-19 03:31:31 +0200

    Recover Trynnie2 use-item cursor-node handler

--- include/wiz8/level_specific_code/Trynnie2.h
+++ include/wiz8/level_specific_code/Trynnie2.h
@@ -3,3 +3,5 @@
 /* Level 0x1a (Trynnie2) helpers, run by InitializeLevelMasterFunctions004D6C50
    under case 0x1a. */
 void EnsureTrynnie2KilledVar004D9D30(void);
+struct W8ItemInstance;
+bool Trynnie2UseItem004D9F60(W8ItemInstance* item);
